d05f130a9f1f905db5bc7478f45c858e2a399be6
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2015-02-02 14:47:44 +0100

    added filters and some formatting

--- src/jmxappl/routes/home.clj
+++ src/jmxappl/routes/home.clj
@@ -15,39 +15,47 @@
 (let [jmx-class-name "java.lang:type=Threading"]
 
     (defn pool-page [host port prefix]
-      (jmx/with-connection {:host host :port port}
-       (defn threadInfo [id]
-            ;(info "threadInfo" id)
-	        (jmx/invoke jmx-class-name :getThreadInfo id))
-		
-         (defn myThread [id]
-            ;(info "myThread" id)
-            (try 
-              (let [this-thread (threadInfo id)]
-	              [id (.get this-thread "threadName") (.get this-thread "threadState")  (.get this-thread "lockName")]) 
-              (catch Exception e (str "caught exception: " (.getMessage e)))))
+      (if 
+        (or (clojure.string/blank? host) (clojure.string/blank? port))
+           (layout/render "pool.html" {:host ""
+	                                      :port 0
+	                                      :prefix prefix
+	                                      :count 0
+	                                      :thread-names nil})
 
-	       (defn allThreadIds []
-	         (jmx/read jmx-class-name :AllThreadIds))
+    (jmx/with-connection {:host host :port port}
+	    (defn threadInfo [id]
+	         ;(info "threadInfo" id)
+		     (jmx/invoke jmx-class-name :getThreadInfo id))
+			
+	      (defn myThread [id]
+	         ;(info "myThread" id)
+	         (try 
+	           (let [this-thread (threadInfo id)]
+		           [id (.get this-thread "threadName") (.get this-thread "threadState")  (.get this-thread "lockName")]) 
+	           (catch Exception e (str "caught exception: " (.getMessage e)))))
 	
-         (let [allThreads (with-local-vars [threads []]                
-	                           (doseq [id (allThreadIds)]
-	                             (let [tt (myThread id)]
-                                 ;(info "tt.1" (nth tt 1) (.startsWith (nth tt 1) prefix))
-;                                 (var-set threads (if (.startsWith (str (nth tt 1)) prefix) (conj @threads tt) threads))))
-                                 (var-set threads (if (.startsWith (str (nth tt 1)) (str prefix)) (conj @threads tt) (set @threads)))))
-;	                               (var-set threads (conj @threads tt))))                                
-                            @threads)]
-
-           (layout/render "pool.html" {:host host
-                                       :port port
-                                       :prefix prefix
-                                       :count (.size allThreads)
-                                       :thread-names allThreads})))))
+		    (defn allThreadIds []
+		      (jmx/read jmx-class-name :AllThreadIds))
+		
+	      (let [allThreads (with-local-vars [threads []]                
+		                        (doseq [id (allThreadIds)]
+		                          (let [tt (myThread id)]
+	                              (var-set threads (if (.startsWith (str (nth tt 1)) (str prefix)) (conj @threads tt) (set @threads)))))
+	                         @threads)]
+	
+	        (layout/render "pool.html" {:host host
+	                                    :port port
+	                                    :prefix prefix
+	                                    :count (if (empty? allThreads) 0 (.size allThreads))
+	                                    :thread-names allThreads}))))
+        
+        )
+      )
 
    
  (defroutes home-routes
-  (GET "/" [] (home-page))
+  (GET "/" [] (pool-page "" 0 ""))
   (GET "/about" [] (about-page))
-  (GET "/pool/:host/:port" [host port] (pool-page host port ""))
+  ;(GET "/pool/:host/:port" [host port] (pool-page host port ""))
   (GET "/pool" [host port prefix] (pool-page host port (str prefix))))
