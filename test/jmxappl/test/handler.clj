5d924377df5e0199ef18a87d5dd254bceb60d2c6
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2015-04-21 12:49:18 +0200

    started adding support of jmx over local connection

--- test/jmxappl/test/handler.clj
+++ test/jmxappl/test/handler.clj
@@ -1,7 +1,9 @@
 (ns jmxappl.test.handler
   (:use clojure.test
         ring.mock.request
-        jmxappl.handler))
+        jmxappl.handler)
+  (:require [clojure.java.jmx :as jmx]))
+
 
 (deftest test-app
   (testing "main route"
@@ -10,4 +12,12 @@
 
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
-      (is (= 404 (:status response))))))
+      (is (= 404 (:status response)))))
+
+  (testing "put new test here"
+      (is true))
+
+  ;(testing "test jmx local connection"
+  ; (jmx/with-connection (jmx/obtain-local-connection "10379")
+  ;        (jmx/mbean "java.lang:type=Memory")))
+  )
